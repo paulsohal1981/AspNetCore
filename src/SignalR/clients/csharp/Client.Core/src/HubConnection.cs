07fad6f4ed6c0b83651a18501657ac8f048f0fce
Stephen Halter <halter73@gmail.com>
2019-05-01 16:32:49 -0700

    More tests

--- src/SignalR/clients/csharp/Client.Core/src/HubConnection.cs
+++ src/SignalR/clients/csharp/Client.Core/src/HubConnection.cs
@@ -1318,8 +1318,9 @@ namespace Microsoft.AspNetCore.SignalR.Client
         {
             var previousReconnectAttempts = 0;
             var reconnectStartTime = DateTime.UtcNow;
+            var retryReason = closeException;
 
-            var nextRetryDelay = GetNextRetryDelay(previousReconnectAttempts++, TimeSpan.Zero);
+            var nextRetryDelay = GetNextRetryDelay(previousReconnectAttempts++, TimeSpan.Zero, retryReason);
 
             if (nextRetryDelay == null)
             {
@@ -1373,6 +1374,8 @@ namespace Microsoft.AspNetCore.SignalR.Client
                 }
                 catch (Exception ex)
                 {
+                    retryReason = ex;
+
                     Log.ReconnectAttemptFailed(_logger, ex);
 
                     if (State != HubConnectionState.Reconnecting)
@@ -1382,7 +1385,7 @@ namespace Microsoft.AspNetCore.SignalR.Client
                     }
                 }
 
-                nextRetryDelay = GetNextRetryDelay(previousReconnectAttempts++, DateTime.UtcNow - reconnectStartTime);
+                nextRetryDelay = GetNextRetryDelay(previousReconnectAttempts++, DateTime.UtcNow - reconnectStartTime, retryReason);
             }
 
             var elapsedTime = DateTime.UtcNow - reconnectStartTime;
@@ -1391,7 +1394,7 @@ namespace Microsoft.AspNetCore.SignalR.Client
             RunCloseEvent(new TimeoutException(message));
         }
 
-        private TimeSpan? GetNextRetryDelay(long previousRetryCount, TimeSpan elapsedTime)
+        private TimeSpan? GetNextRetryDelay(long previousRetryCount, TimeSpan elapsedTime, Exception retryReason)
         {
             try
             {
@@ -1399,6 +1402,7 @@ namespace Microsoft.AspNetCore.SignalR.Client
                 {
                     PreviousRetryCount = previousRetryCount,
                     ElapsedTime = elapsedTime,
+                    RetryReason = retryReason,
                 });
             }
             catch (Exception ex)
